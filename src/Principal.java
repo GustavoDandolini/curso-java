1ddfd32a4d43f98c2c37b95127a0e8e99c61cd74
Gustavo Dandolini <dandolinigustavo@gmail.com>
2026-05-14 19:00:45 -0300

    screenmatch 1.0.1

--- src/Principal.java
+++ src/Principal.java
@@ -4,15 +4,16 @@ public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
         meuFilme.setNome("Kimetsu no yaiba infinite castle part 2");
-        meuFilme.setAnoDeLacamento(2025);
+        meuFilme.setAnoDeLancamento(2026);
         meuFilme.setIncluidoNoPlano(true);
         meuFilme.setDuracaoEmMinutos(180);
 
 
         meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(8);
         meuFilme.avalia(10);
+        meuFilme.avalia(9);
+        meuFilme.avalia(10);
+        meuFilme.avalia(2);
 
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("Média de avaliação do filme: " +meuFilme.pegaMedia());
--- src/br/com/gustavo/screenmatch/modelos/Filme.java
+++ src/br/com/gustavo/screenmatch/modelos/Filme.java
@@ -1,62 +1,65 @@
 package br.com.gustavo.screenmatch.modelos;
 
 public class Filme {
+
     private String nome;
-    private int anoDeLacamento;
+    private int anoDeLancamento;
+    private int duracaoEmMinutos;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " +nome);
+        System.out.println("Ano de lançamento: " +anoDeLancamento);
+        System.out.println("Duração em minutos: " +duracaoEmMinutos);
+        System.out.println("Incluído no plano: " +incluidoNoPlano);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
 
     public String getNome() {
-        return nome;
-    }
-
-    public int getAnoDeLacamento() {
-        return anoDeLacamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
+        return this.nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public void setAnoDeLacamento(int anoDeLacamento) {
-        this.anoDeLacamento = anoDeLacamento;
+    public int getAnoDeLancamento() {
+        return this.anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean getIncluidoNoPlano() {
+        return this.incluidoNoPlano;
     }
 
     public void setIncluidoNoPlano(boolean incluidoNoPlano) {
         this.incluidoNoPlano = incluidoNoPlano;
     }
 
+    public int getDuracaoEmMinutos() {
+        return this.duracaoEmMinutos;
+    }
+
     public void setDuracaoEmMinutos(int duracaoEmMinutos) {
         this.duracaoEmMinutos = duracaoEmMinutos;
     }
 
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLacamento);
-        System.out.println("Duração em minutos: " + duracaoEmMinutos);
-        System.out.println("Incluído no plano: " + incluidoNoPlano);
+    public int getTotalDeAvaliacoes() {
+        return this.totalDeAvaliacoes;
     }
 
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-    }
 }
